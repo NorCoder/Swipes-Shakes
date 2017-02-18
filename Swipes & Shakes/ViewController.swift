fb2a9d66027233649cdd8391295daabee8b41d51
Kjetil Korsveien <korsveien@me.com>
2017-02-18 23:20:58 +0100

    Initial Commit

---
+++ Swipes & Shakes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipes & Shakes
+//
+//  Created by Kjetil Korsveien on 18.02.2017.
+//  Copyright © 2017 Kjetil Korsveien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
